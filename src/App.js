5d802ab01804a470016dad28e587d248807a31dc
Joel.M <joel.moran@students.beaminstitute.org>
2024-08-06 15:52:05 +0200

    jm:initial commit for the final project Give and Take Hub

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import GiveItem from './pages/GiveItem';
+import SearchItems from './pages/SearchItems';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Give and Take Hub</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/give" element={<GiveItem />} />
+          <Route path="/search" element={<SearchItems />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/SearchItems.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SearchItems() {
+  return <h2>Search Items</h2>;
+}
+
+export default SearchItems;
