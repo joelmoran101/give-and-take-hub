2466686e5cf5712be26298381dd779e15a84c227
Joel.M <joel.moran@students.beaminstitute.org>
2024-11-05 16:14:18 +0100

    jm:refactored the BrowseItems files and made it responsive to the device view width.

--- src/pages/Terms/SimplifiedTerms.tsx
+++ src/pages/Terms/SimplifiedTerms.tsx
@@ -34,13 +34,13 @@ const SimplifiedTerms: React.FC = () => {
 
       <div className="terms-content">
         <h1 className="flex items-center">
-          <Users className="h-8 w-8 text-primary flex-shrink-0 mr-4" />
+          <Users className="icon h-8 w-8 text-secondary flex-shrink-0 mr-4" />
           <span>{t('Community based Terms and Conditions')}</span>
         </h1>
         <p>{t('Community based Terms p')}</p>
 
         <h2 className="flex items-center mt-6">
-          <Heart className="h-8 w-8 text-red-500 flex-shrink-0 mr-4" />
+          <Heart className="icon h-8 w-8 text-info flex-shrink-0 mr-4" />
           <span>{t('trust.title')}</span>
         </h2>
         <ul>
@@ -48,7 +48,7 @@ const SimplifiedTerms: React.FC = () => {
         </ul>
 
         <h2 className="flex items-center mt-6">
-          <UserCheck className="h-8 w-8 text-green-500 flex-shrink-0 mr-4" />
+          <UserCheck className="icon h-8 w-8 text-success flex-shrink-0 mr-4" />
           <span>{t('honesty.title')}</span>
         </h2>
         <ul>
@@ -56,7 +56,7 @@ const SimplifiedTerms: React.FC = () => {
         </ul>
 
         <h2 className="flex items-center mt-6">
-          <Lightbulb className="h-8 w-8 text-yellow-500 flex-shrink-0 mr-4" />
+          <Lightbulb className="icon h-8 w-8 text-warning color-yellow-500 flex-shrink-0 mr-4" />
           <span>{t('Core Concept')}</span>
         </h2>
 
@@ -68,7 +68,7 @@ const SimplifiedTerms: React.FC = () => {
 
         <div className="privacy-note mt-6">
           <h2 className="flex items-center">
-            <Shield className="h-8 w-8 text-primary flex-shrink-0 mr-4" />
+            <Shield className="icon h-8 w-8 text-primary flex-shrink-0 mr-4" />
             <span>{t('privacy.title')}</span>
           </h2>
           <p>{t('privacy.content')}</p>
